3a4ee10a3a5592eb1f58c5c2f8030ee82766ebea
ChrisJacket <fratelepana@gmail.com>
2025-08-01 15:12:52 +0300

    Text boxes move out of the way of the mouse cursor, and hide themselves as needed

--- Scripts/location_clickable.gd
+++ Scripts/location_clickable.gd
@@ -1,17 +1,22 @@
 extends Area2D
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+@onready var textBox: CanvasLayer = $Text_Box
 @export_file("*.tscn") var targetScene = "<Select target>"
+var cursorLocation = "down" # bottom quarter of the screen or not
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
 	input_event.connect(_input_event)
+	textBox.hide()
 	
 func _on_mouse_entered():
-	animation_player.play("scaleAnim")
-
+	animationPlayer.play("scaleAnim")
+	textBox.show()
+	
 func _on_mouse_exited():
-	animation_player.play_backwards("scaleAnim")
+	animationPlayer.play_backwards("scaleAnim")
+	textBox.hide()
 	
 func change_scene(target: String, viewport: Node):
 	viewport.get_tree().change_scene_to_file(target)
@@ -21,3 +26,14 @@ func _input_event(viewport: Node, event: InputEvent, _shape_idx: int):
 		match event.button_index:
 			MOUSE_BUTTON_LEFT:
 				change_scene(targetScene, viewport)
+
+func _process(delta: float) -> void:
+	var screenQuarter = get_viewport().get_visible_rect().size.y / 4
+	var mousePos = get_global_mouse_position()
+	
+	if(mousePos.y < screenQuarter and cursorLocation == "up"):
+		textBox.transform.origin = Vector2(0, -5)
+		cursorLocation = "down"
+	elif(mousePos.y >= 3 * screenQuarter and cursorLocation == "down"):
+		textBox.transform.origin = Vector2(0, -150)
+		cursorLocation = "up"
