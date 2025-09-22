bbcca0305feea97282d1df2303e5d266beb6b3e5
ChrisJacket <fratelepana@gmail.com>
2025-09-22 12:00:25 +0300

    Conversation menu code for reading dialogue from file, and displaying it on screen

--- Scripts/conv_menu.gd
+++ Scripts/conv_menu.gd
@@ -1,18 +1,49 @@
 extends Node2D
 @onready var richTextLabel: RichTextLabel = $RichTextLabel
 @onready var vBoxContainer: VBoxContainer = $VBoxContainer
-@export_file var TargetCharacter = "<Select target>"
-var IsTalking: bool
+@onready var animPlayer: AnimationPlayer = $AnimationPlayer
 
-# visibility needs to be applied in ready and process, to make sure text doesn't flash on
-# screen during scene change.
-# this can be alleviated using a transition screen, at which point
-# the ready function should be removed.
+@export_file var TargetCharacter = "<Select target>"
+@export_file var TargetDialogueFile = "<Select target>"
+@export var dialogueSpeed = 25.0
+
+var isTalking: bool
+var Dialogue: PackedStringArray
+var speechIndex = 0
+
+func Load(Target: String) -> PackedStringArray:
+	var file = FileAccess.open(Target, FileAccess.READ)
+	return file.get_as_text().split(">", false)
 
 func _ready() -> void:
-	richTextLabel.visible = IsTalking
-	vBoxContainer.visible = not IsTalking
+	# visibility needs to be applied in ready and process, to make sure text doesn't flash on
+	# screen during scene change.
+	# this can be alleviated using a transition screen, at which point
+	# the ready function visibility modifiers should be removed.
+	richTextLabel.visible = isTalking
+	vBoxContainer.visible = not isTalking
 	
-func _process(delta: float) -> void:
-	richTextLabel.visible = IsTalking
-	vBoxContainer.visible = not IsTalking
+	Dialogue = Load(TargetDialogueFile)
+	assert(!Dialogue.is_empty(), "No dialogue was loaded!")
+
+func Speak(content: PackedStringArray):
+	if speechIndex >= content.size():
+		return
+	richTextLabel.text = content[speechIndex]
+	animPlayer.speed_scale = dialogueSpeed / richTextLabel.text.length()
+	
+	if Input.is_action_just_pressed("Continue"):
+		if animPlayer.is_playing(): 
+			animPlayer.seek(animPlayer.get_animation(animPlayer.current_animation).length, true)
+		else:
+			animPlayer.play()
+			speechIndex += 1
+	
+	# FIXME: Ultimul element de dialog apare de doua ori la rand
+	
+func _process(_delta: float) -> void:
+	richTextLabel.visible = isTalking
+	vBoxContainer.visible = not isTalking
+	
+	isTalking = true
+	Speak(Dialogue)
