945e4ece041c71c6855c866a98381a185984fefa
Velici Richard-Alexandru <velici.richard@gmail.com>
2025-10-03 21:17:00 +0300

    JSON storage structure implemented in traits_selection and random story selection for every choice. Text animation and choice box on/off visibility also added.

--- Scripts/test_script.gd
+++ Scripts/test_script.gd
@@ -13,8 +13,11 @@ var Personality : String = ""
 var Profession : String = ""
 var Passives : Array[String] = []
 var Items : Array[String] = []
+var wearableItems : Array[String] = []
 
 var isFirstTime = false
+var DialogueDict
+var rand = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	choice_type.choice_1.connect(ChoiceSelected)
@@ -27,7 +30,8 @@ func _ready() -> void:
 	choice_type.choice_8.connect(ChoiceSelected)
 	choice_type.choice_9.connect(ChoiceSelected)
 	choice_type.choice_10.connect(ChoiceSelected)
-	label.text = fileReader.Load()[0]
+	DialogueDict = fileReader.LoadJson()
+	label.text = fileReader.Extract(DialogueDict, "first")[rand.randi_range(0,2)]
 	tratits_selected.connect(addTraits)
 	Player.trait_changed.connect(_selected_traits)
 	
@@ -43,26 +47,29 @@ func _selected_traits():
 	get_tree().change_scene_to_file("res://Scenes/world_map.tscn")
 
 func addTraits():
-	Player.initTraits(Upbringing,Personality,Profession,Passives,Items)
+	Player.initCharacter(Upbringing,Personality,Profession,Passives, wearableItems, Items)
 
 func ChoiceSelected(choice):
 	if (Upbringing == ""):
 		Upbringing = choice
-		label.text = fileReader.Load()[1]
+		label.text = fileReader.Extract(DialogueDict, "second")[rand.randi_range(0,2)]
 		animation_player.play("text_show")
 	elif (Personality == ""):
 		Personality = choice
-		label.text = fileReader.Load()[2]
+		label.text = fileReader.Extract(DialogueDict, "third")[rand.randi_range(0,2)]
 		animation_player.play("text_show")
 	elif (Profession == ""):
 		Profession = choice
-		label.text = fileReader.Load()[3]
+		label.text = fileReader.Extract(DialogueDict, "fourth")[rand.randi_range(0,2)]
 		animation_player.play("text_show")
 	elif (Passives == []):
 		Passives = choice
-		label.text = fileReader.Load()[4]
+		label.text = fileReader.Extract(DialogueDict, "fifth")[rand.randi_range(0,2)]
 		animation_player.play("text_show")
-	elif (Items == []):
-		Items = choice
+	elif (Items == [] or wearableItems == []):
+		if Global.wearableItems.has(choice[0]):
+			wearableItems = choice
+		else:
+			Items = choice
 		tratits_selected.emit()
 	print(choice)
